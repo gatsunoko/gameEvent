67da4f789dee7e8bc6fd8f07fb18e9346c716eb5
gatsuonrails <gatsuonrails@gmail.com>
2020-06-17 02:54:05 +0900

    トップページに５件表示するように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @event_details = EventDetail.where(latest: true)
+                                .where('date > ?', Date.today - 1)
+                                .order(date: :asc)
+                                .limit(5)
+                                .offset(0)
   end
 end
