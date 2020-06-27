46690423798e1d3068e77659bd0a47b81e8a2bd8
gatsuonrails <gatsuonrails@gmail.com>
2020-06-27 19:26:01 +0900

    見た目調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,20 @@ function previewFile(name) {
     $('img[name="' + name + '"]')[0].alt = ""//何も選択されなかったらプレビュー空白にする
   }
 }
+
+//ナビバーを下にスクロールしたら消えて少しでも上にスクロールしたらだす
+$(window).on('load', function(){
+  var menuHeight = 56;
+  var startPos = 0;
+  $(window).scroll(function(){
+    var currentPos = $(this).scrollTop();
+    if (currentPos > startPos) {
+      if($(window).scrollTop() >= 200) {
+        $("#navbar").css("top", "-" + menuHeight + "px");
+      }
+    } else {
+      $("#navbar").css("top", 0 + "px");
+    }
+    startPos = currentPos;
+  });
+});
